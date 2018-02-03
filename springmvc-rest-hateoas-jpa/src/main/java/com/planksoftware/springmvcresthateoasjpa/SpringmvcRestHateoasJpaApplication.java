d810d649f2a111a4cf40d8d1d8bc3d8f7dcc0634
jp052 <jan.p2001@gmail.com>
2018-02-03 13:13:39 +1030

    springmvc-rest-hateoas-jpa init

---
+++ springmvc-rest-hateoas-jpa/src/main/java/com/planksoftware/springmvcresthateoasjpa/SpringmvcRestHateoasJpaApplication.java
@@ -0,0 +1,12 @@
+package com.planksoftware.springmvcresthateoasjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcRestHateoasJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcRestHateoasJpaApplication.class, args);
+	}
+}
---
+++ springmvc-rest-hateoas-jpa/src/test/java/com/planksoftware/springmvcresthateoasjpa/SpringmvcRestHateoasJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.planksoftware.springmvcresthateoasjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringmvcRestHateoasJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
