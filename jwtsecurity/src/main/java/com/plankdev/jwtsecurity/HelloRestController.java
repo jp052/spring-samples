dd0811c69072c02fb6be80a7208e7088fd526168
jp052 <jan.p2001@gmail.com>
2018-02-14 23:54:35 +1030

    jwtsecurity: init

---
+++ jwtsecurity/src/main/java/com/plankdev/jwtsecurity/HelloRestController.java
@@ -0,0 +1,21 @@
+package com.plankdev.jwtsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+    @GetMapping("/public")
+    public String helloPublic() {
+        return "Hello from public rest resource";
+    }
+
+    @TokenRequired
+    @GetMapping("/private")
+    public String helloPrivate() {
+        return "Hello from private rest resource";
+    }
+}
---
+++ jwtsecurity/src/main/java/com/plankdev/jwtsecurity/SecurityRestController.java
@@ -0,0 +1,43 @@
+package com.plankdev.jwtsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+@RestController
+public class SecurityRestController {
+
+    @Autowired
+    SecurityService securityService;
+
+    @ResponseBody
+    @RequestMapping("/security/generate/token")
+    public Map<String, Object> generateToken(@RequestParam(value="subject") String subject){
+
+        String token = securityService.createToken(subject, (Integer.MAX_VALUE));
+
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("result", token);
+
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("/security/get/subject")
+    public Map<String, Object> getSubject(@RequestParam(value="token") String token){
+
+        String subject = securityService.getSubject(token);
+
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("result", subject);
+
+        return map;
+    }
+
+}
