34cb53dee9e31189e5682db2407a5539a30da8f1
jp052 <jan.p2001@gmail.com>
2018-03-18 15:37:30 +1030

    jwtsecurity: Vehicle Controller Test added

---
+++ jwtsecurity/src/test/java/com/plankdev/jwtsecurity/api/VehicleRestControllerTest.java
@@ -0,0 +1,68 @@
+package com.plankdev.jwtsecurity.api;
+
+import com.plankdev.jwtsecurity.security.dataaccess.Application;
+import com.plankdev.jwtsecurity.testutils.BaseRestControllerTest;
+import org.junit.Test;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+
+public class VehicleRestControllerTest extends BaseRestControllerTest {
+
+    @Test
+    public void dummyTest() {
+    }
+
+    @Test
+    @WithUserDetails(value = "user", userDetailsServiceBeanName = "customUserDetailsService")
+    public void createVehicleWorksWhenAllValuesSet() throws Exception {
+        //assemble
+        Application app = createApplication();
+
+        String VEHICLE_NAME_EXPECTED = "car1";
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName(VEHICLE_NAME_EXPECTED);
+        vehicle.setApplication(app);
+
+        String vehicleJson = jsonUtils.pojoToJson(vehicle);
+
+        //action
+        ResultActions performRequest = mockMvc.perform(post("/api/v1/vehicles")
+                .contentType(contentType)
+                .content(vehicleJson))
+                .andDo(print());
+
+        //assert
+        performRequest
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value(VEHICLE_NAME_EXPECTED));
+    }
+
+    private Application createApplication() throws Exception {
+        final String APP_NAME_EXPECTED = "myApp1";
+        Application newApp = new Application();
+        newApp.setName(APP_NAME_EXPECTED);
+        String applicationJson = jsonUtils.pojoToJson(newApp);
+
+        MvcResult createApplicationResult = mockMvc.perform(post("/api/users/1/applications")
+                .contentType(contentType)
+                .content(applicationJson))
+                .andReturn();
+
+        String applicationJsonAsString = createApplicationResult.getResponse().getContentAsString();
+        Application createdApp = jsonUtils.jsonStringToPojo(applicationJsonAsString, Application.class);
+        return createdApp;
+    }
+
+
+    //createVehicleFailsWhenApplicationDoesNotMatchApiKey
+}
